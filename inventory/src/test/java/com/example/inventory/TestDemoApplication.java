f51e6f102e8b100f2b710b7fbd9e83c6f3e05cf7
Huseyin BABAL <huseyinbabal88@gmail.com>
2023-07-25 19:26:38 +0300

    initial

---
+++ inventory/src/test/java/com/example/inventory/TestDemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestDemoApplication {
+
+	@Bean
+	@ServiceConnection
+	PostgreSQLContainer<?> postgresContainer() {
+		return new PostgreSQLContainer<>("postgres:latest");
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(DemoApplication::main).with(TestDemoApplication.class).run(args);
+	}
+
+}
